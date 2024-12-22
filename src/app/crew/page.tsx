20d397e8ca9b23812f4c7ce55f71f6de6093b4ef
Fatkharrofiqi <achmadfatkharrofiqi404@gmail.com>
2024-12-22 11:53:58 +0700

    feat: responsive tablet mode in page crew

--- src/app/crew/page.tsx
+++ src/app/crew/page.tsx
@@ -52,42 +52,51 @@ export default function Crew() {
   const crew: Crew | null = crewData.find((v) => v.id === activeCrew) || null;
 
   return (
-    <div className="container mx-auto my-12 grid grid-cols-12 gap-[1.875rem]">
+    <div className="container md:m-10 lg:mx-auto lg:my-12 grid grid-cols-12 md:gap-2 lg:gap-[1.875rem]">
       <div className="col-span-full h-full flex flex-col gap-6">
         <div role="heading" className="flex gap-6">
-          <span className="font-bold text-[1.75rem] tracking-[0.295rem] text-white/25 leading-[2.125rem]">
+          <span className="font-bold md:text-[1.25rem] md:tracking-[0.1018rem] md:leading-[1.5rem] lg:text-[1.75rem] lg:tracking-[0.295rem] text-white/25 lg:leading-[2.125rem]">
             02
           </span>
-          <p className="text-[1.75rem] tracking-[0.25rem] leading-[2.125rem]">
+          <p className="md:text-[1.25rem] md:tracking-[0.181525rem] md:leading-[1.5rem] lg:text-[1.75rem] lg:tracking-[0.25rem] lg:leading-[2.125rem]">
             MEET YOUR CREW
           </p>
         </div>
-        <div role="contentinfo" className="flex-1 flex gap-8">
-          <div role="contentinfo" className="flex flex-col flex-1 gap-10">
+        <div
+          role="contentinfo"
+          className="grid md:grid-cols-1 lg:grid-cols-2 flex-1 gap-8"
+        >
+          <div
+            role="contentinfo"
+            className="flex flex-col md:mx-auto lg:mx-0 md:max-w-[32rem] lg:max-w-full flex-1 gap-10"
+          >
             <div
               id="explanation"
               className="flex flex-1 flex-col gap-6 justify-center"
             >
               <div id="rank_and_name" className="flex flex-col gap-4">
-                <p className="font-['Bellefair'] text-[2rem] leading-[2.3125rem] text-white/50 uppercase">
+                <p className="font-['Bellefair'] md:text-center lg:text-left md:text-2xl lg:text-[2rem] md:leading-[1.1669] lg:leading-[2.3125rem] text-white/50 uppercase">
                   {crew?.rank}
                 </p>
-                <p className="font-['Bellefair'] text-[3.5rem] leading-[4rem] uppercase">
+                <p className="font-['Bellefair'] md:text-center lg:text-left md:text-[2.5rem] lg:text-[3.5rem] md:leading-[1.15] lg:leading-[4rem] uppercase">
                   {crew?.name}
                 </p>
               </div>
               <div id="description">
-                <p className="text-blue-200 font-['Barlow'] text-[1.125rem] leading-[2rem]">
+                <p className="text-blue-200 font-['Barlow'] md:text-center lg:text-left md:text-base lg:text-[1.125rem] md:leading-[1.8129] lg:leading-[2rem]">
                   {crew?.description}
                 </p>
               </div>
             </div>
-            <div id="pagination" className="flex gap-2 pb-12">
+            <div
+              id="pagination"
+              className="flex gap-2 lg:pb-12 md:justify-center lg:justify-start"
+            >
               <ul className="flex gap-10">
                 {crewData.map((v) => (
                   <li key={v.id}>
                     <button
-                      className={`size-[0.9375rem] rounded-full cursor-pointer transition-all duration-200 ${
+                      className={`md:size-[0.625rem] lg:size-[0.9375rem] rounded-full cursor-pointer transition-all duration-200 ${
                         activeCrew === v.id
                           ? "bg-white"
                           : "bg-white/20 hover:bg-white/50"
@@ -103,7 +112,7 @@ export default function Crew() {
             role="img"
             className="flex flex-1 justify-center items-center relative"
           >
-            <div className="h-[42.25rem] relative">
+            <div className="md:h-[35rem] lg:h-[42.25rem] relative">
               <img
                 src={crew?.image}
                 alt={crew?.name}
@@ -120,7 +129,15 @@ export default function Crew() {
           src="/assets/crew/background-crew-desktop.jpg"
           alt="Background"
           fill
-          className="object-cover"
+          className="object-cover md:hidden lg:block"
+          priority
+        />
+
+        <Image
+          src="/assets/crew/background-crew-tablet.jpg"
+          alt="Background"
+          fill
+          className="object-cover md:block lg:hidden"
           priority
         />
         {/* Dark overlay */}
